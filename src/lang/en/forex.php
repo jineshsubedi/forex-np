f0db036daa0647dd6eecb96192db68d2f3254d43
jinesh subedi <jinesh1094@gmail.com>
2022-09-29 12:28:38 +0545

    update

--- src/lang/en/forex.php
+++ src/lang/en/forex.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'description' => 'Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta dapibus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.',
+    'description' => 'This is a Nepal Forex Exchange.',
 ];
